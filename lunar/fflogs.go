e28acf3cdc1733b861f619d342979fa2264b46ab
Nigel Hearn <nhearn@mmm.com>
2021-05-02 20:16:06 -0500

    Initial commit

---
+++ lunar/fflogs.go
@@ -0,0 +1,347 @@
+package lunar
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	graphQLApi = "https://www.fflogs.com/api/v2/client"
+	authURL    = "https://www.fflogs.com/oauth/authorize"
+	tokenURL   = "https://www.fflogs.com/oauth/token"
+
+	//Use this query to obtain the ids, start and end times of all the fights in the report.
+	//It also grabs the name of the fight, which is usually the name of the boss.
+	//We need the start and end time to retrieve information about damage downs and deaths.
+	fightsQuery = `
+	query($report: String!) {
+		reportData {
+			report(code: $report) {
+				fights {
+					id
+					name
+					startTime
+					endTime
+				}
+			}
+		}
+	}
+	`
+
+	query = `
+	query($report: String!, $start: Float!, $end: Float!, $fid: [Int!]!) {
+		reportData {
+			report(code: $report) {
+				...composition
+				...rdpsRanking
+				...dmgDowns
+			}
+		}
+	}
+
+	fragment dmgDowns on Report {
+		events(startTime: $start, endTime: $end, dataType: Debuffs, abilityID: 1002092) {
+		  data
+		}
+	  }
+	  
+	  fragment composition on Report {
+		composition: table(fightIDs: $fid, startTime: $start, endTime: $end, dataType: Summary)
+	  }
+	  
+	  fragment rdpsRanking on Report {
+		rankings(fightIDs: $fid, playerMetric: rdps)
+	  }
+	  
+	`
+)
+
+var (
+	JobTickers = map[string]string{
+		"Paladin":     "PLD",
+		"Warrior":     "WAR",
+		"Dark Knight": "DRK",
+		"Gunbreaker":  "GNB",
+
+		"Monk":    "MNK",
+		"Dragoon": "DRG",
+		"Ninja":   "NIN",
+		"Samurai": "SAM",
+
+		"White Mage":  "WHM",
+		"Scholar":     "SCH",
+		"Astrologian": "AST",
+
+		"Bard":      "BRD",
+		"Machinist": "MCH",
+		"Dancer":    "DNC",
+
+		"Black Mage": "BLM",
+		"Summoner":   "SMN",
+		"Red Mage":   "RDM",
+		"Blue Mage":  "BLU",
+	}
+)
+
+type CompositionPlayer struct {
+	Name string `json:"name"`
+	Job  string `json:"type"`
+	ID   int    `json:"id"`
+}
+
+type DeathEvent struct {
+	PlayerName string `json:"name"`
+	PlayerID   int    `json:"id"`
+}
+
+type DamageDowns struct {
+	DebuffMode string `json:"type"`
+	TargetID   int    `json:"targetID"`
+}
+
+type Fight struct {
+	ID        int    `json:"id"`
+	BossName  string `json:"name"`
+	StartTime int    `json:"startTime"`
+	EndTime   int    `json:"endTime"`
+}
+
+type RdpsRankingCharacter struct {
+	PlayerName  string  `json:"name"`
+	Rdps        float64 `json:"amount"`
+	FightRank   string  `json:"rank"`
+	BestRank    string  `json:"best"`
+	RankPercent int     `json:"rankPercent"`
+	//Class2 is to detect if the player is a combined healer or tank ranking
+	Class2 *string `json:"class_2"`
+}
+
+type SpeedRanking struct {
+	Rank        string `json:"rank"`
+	TotalParses int    `json:"totalParses"`
+	RankPercent int    `json:"rankPercent"`
+}
+
+type FightsResponse struct {
+	QueryData struct {
+		ReportData struct {
+			Report struct {
+				Fights []Fight `json:"fights"`
+			} `json:"report"`
+		} `json:"reportData"`
+	} `json:"data"`
+}
+
+type QueryResponse struct {
+	Data struct {
+		ReportData struct {
+			Report struct {
+				Composition struct {
+					CompositionData struct {
+						TotalTime   float64             `json:"totalTime"`
+						Party       []CompositionPlayer `json:"composition"`
+						DeathEvents []DeathEvent        `json:"deathEvents"`
+					} `json:"data"`
+				} `json:"composition"`
+				Rankings struct {
+					RankingData []RankingData `json:"data"`
+				} `json:"rankings"`
+				DamageDownEvents struct {
+					DmgDownInformation []DamageDowns `json:"data"`
+				} `json:"events"`
+			} `json:"report"`
+		} `json:"reportData"`
+	} `json:"data"`
+}
+
+type RankingData struct {
+	Roles struct {
+		Tanks struct {
+			TankCharacters []RdpsRankingCharacter `json:"characters"`
+		} `json:"tanks"`
+		Healers struct {
+			HealerCharacters []RdpsRankingCharacter `json:"characters"`
+		} `json:"healers"`
+		DPS struct {
+			DPSCharacters []RdpsRankingCharacter `json:"characters"`
+		} `json:"dps"`
+	} `json:"roles"`
+	SpeedRank SpeedRanking `json:"speed"`
+}
+
+type DeconstructedQueryResponse struct {
+	TotalTime float64
+	Deaths    map[string]int
+	Players   map[string]RdpsRankingCharacter
+	//RdpsRankings []RdpsRankingCharacter
+	SpeedRank   SpeedRanking
+	DamageDowns map[string]int
+	Jobs        map[string]string
+}
+
+type FFLogsClient struct {
+	client      *http.Client
+	AccessToken Token
+}
+
+type Token struct {
+	AccessToken string `json:"access_token"`
+}
+
+func NewFFLogsClient() FFLogsClient {
+	hc := http.Client{Timeout: time.Second * 10}
+
+	//Retrieve the access token using Oauth2
+	clientID := os.Getenv("LUNAR_HYDAELYN_ID")
+	clientSecret := os.Getenv("LUNAR_HYDAELYN_SECRET")
+
+	multiForm := url.Values{}
+	multiForm.Add("grant_type", `client_credentials`)
+
+	req, err := http.NewRequest(http.MethodPost, tokenURL, strings.NewReader(multiForm.Encode()))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.SetBasicAuth(clientID, clientSecret)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := hc.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	bodyBytes, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var token Token
+	json.Unmarshal(bodyBytes, &token)
+	return FFLogsClient{
+		client:      &http.Client{Timeout: time.Second * 10},
+		AccessToken: token,
+	}
+}
+
+func (c FFLogsClient) runRequest(query string, variables map[string]interface{}) []byte {
+	requestBody := struct {
+		Query     string                 `json:"query"`
+		Variables map[string]interface{} `json:"variables"`
+	}{
+		Query:     query,
+		Variables: variables,
+	}
+
+	payload, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body := bytes.NewReader(payload)
+	req, err := http.NewRequest(http.MethodGet, graphQLApi, body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprint("Bearer ", c.AccessToken.AccessToken))
+
+	hc := http.Client{Timeout: time.Second * 10}
+	resp, err := hc.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return bodyBytes
+}
+
+func (c FFLogsClient) GetLastFightInfo(report string) (*Fight, int) {
+	requestVariables := map[string]interface{}{
+		"report": report,
+	}
+
+	var response FightsResponse
+	json.Unmarshal(c.runRequest(fightsQuery, requestVariables), &response)
+
+	fights := response.QueryData.ReportData.Report.Fights
+	numFights := len(fights)
+
+	if numFights > 0 {
+		return &fights[numFights-1], numFights
+	} else {
+		return nil, 0
+	}
+}
+
+func (c FFLogsClient) GrabReportInfo(report string, fight Fight) DeconstructedQueryResponse {
+	requestVariables := map[string]interface{}{
+		"report": report,
+		"start":  fight.StartTime,
+		"end":    fight.EndTime,
+		"fid":    [1]int{fight.ID},
+	}
+
+	var response QueryResponse
+	json.Unmarshal(c.runRequest(query, requestVariables), &response)
+
+	var dr DeconstructedQueryResponse
+	dr.TotalTime = response.Data.ReportData.Report.Composition.CompositionData.TotalTime
+	dr.SpeedRank = response.Data.ReportData.Report.Rankings.RankingData[0].SpeedRank
+
+	dr.Players = make(map[string]RdpsRankingCharacter)
+	for _, player := range response.Data.ReportData.Report.Rankings.RankingData[0].Roles.Tanks.TankCharacters {
+		if player.Class2 == nil {
+			dr.Players[player.PlayerName] = player
+		}
+	}
+	for _, player := range response.Data.ReportData.Report.Rankings.RankingData[0].Roles.Healers.HealerCharacters {
+		if player.Class2 == nil {
+			dr.Players[player.PlayerName] = player
+		}
+	}
+	for _, player := range response.Data.ReportData.Report.Rankings.RankingData[0].Roles.DPS.DPSCharacters {
+		dr.Players[player.PlayerName] = player
+	}
+
+	//dr.RdpsRankings = append(dr.RdpsRankings, response.Data.ReportData.Report.Rankings.RankingData[0].Roles.Tanks.TankCharacters...)
+	//dr.RdpsRankings = append(dr.RdpsRankings, response.Data.ReportData.Report.Rankings.RankingData[0].Roles.Healers.HealerCharacters...)
+	//dr.RdpsRankings = append(dr.RdpsRankings, response.Data.ReportData.Report.Rankings.RankingData[0].Roles.DPS.DPSCharacters...)
+	//sort.Sort(byRdps(dr.RdpsRankings))
+
+	dr.Deaths = make(map[string]int)
+	dr.DamageDowns = make(map[string]int)
+	for _, s := range dr.Players {
+		dr.Deaths[s.PlayerName] = 0
+		dr.DamageDowns[s.PlayerName] = 0
+	}
+
+	for _, s := range response.Data.ReportData.Report.Composition.CompositionData.DeathEvents {
+		dr.Deaths[s.PlayerName]++
+	}
+
+	dr.Jobs = make(map[string]string)
+	for _, player := range response.Data.ReportData.Report.Composition.CompositionData.Party {
+		dr.Jobs[player.Name] = JobTickers[player.Job]
+	}
+
+	return dr
+}
---
+++ main.go
@@ -0,0 +1,116 @@
+// package main
+
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"os"
+// 	"os/signal"
+// 	"strings"
+// 	"syscall"
+
+// 	"github.com/bwmarrin/discordgo"
+// 	"github.com/drakargx/lunar-hydaelyn/lunar"
+// )
+
+// // Variables used for command line parameters
+// var (
+// 	Token string
+// )
+
+// func init() {
+
+// 	flag.StringVar(&Token, "t", "", "Bot Token")
+// 	flag.Parse()
+// }
+
+// func main() {
+// 	Token := "ODMzNTM0ODcxNjAyOTg3MDIw.YHzv1w.nK84CXuO6Vq5e8ZLkmYEL1GpS9o"
+// 	// Create a new Discord session using the provided bot token.
+// 	dg, err := discordgo.New("Bot " + Token)
+// 	if err != nil {
+// 		fmt.Println("error creating Discord session,", err)
+// 		return
+// 	}
+
+// 	// Register the messageCreate func as a callback for MessageCreate events.
+// 	dg.AddHandler(messageCreate)
+
+// 	// In this example, we only care about receiving message events.
+// 	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+// 	// Open a websocket connection to Discord and begin listening.
+// 	err = dg.Open()
+// 	if err != nil {
+// 		fmt.Println("error opening connection,", err)
+// 		return
+// 	}
+
+// 	// Wait here until CTRL-C or other term signal is received.
+// 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+// 	sc := make(chan os.Signal, 1)
+// 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+// 	<-sc
+
+// 	// Cleanly close down the Discord session.
+// 	dg.Close()
+// }
+
+// // This function will be called (due to AddHandler above) every time a new
+// // message is created on any channel that the authenticated bot has access to.
+// func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+// 	// Ignore all messages created by the bot itself
+// 	// This isn't required in this specific example but it's a good practice.
+// 	if m.Author.ID == s.State.User.ID {
+// 		return
+// 	}
+// 	// If the message is "ping" reply with "Pong!"
+// 	if strings.Contains(m.Content, "fflogs.com/reports/") {
+
+// 		reportID := ""
+// 		segments := strings.Split(m.Content, "/")
+// 		for i, value := range segments {
+// 			if value == "reports" {
+// 				reportID = segments[i+1]
+// 				break
+// 			}
+// 		}
+
+// 		if reportID != "" {
+// 			client := lunar.NewFFLogsClient()
+
+// 			fight, _ := client.GetLastFightInfo(reportID)
+// 			if fight != nil {
+// 				s.ChannelMessageSend(m.ChannelID, fight.BossName)
+// 			} else {
+// 				s.ChannelMessageSend(m.ChannelID, "nil fight")
+// 			}
+
+// 		}
+
+// 	}
+
+// 	// If the message is "pong" reply with "Ping!"
+// 	if m.Content == "pong" {
+// 		s.ChannelMessageSend(m.ChannelID, "Ping!")
+// 	}
+// }
+
+package main
+
+import (
+	//"fmt"
+
+	"github.com/drakargx/lunar-hydaelyn/lunar"
+)
+
+func main() {
+
+	client := lunar.NewFFLogsClient()
+
+	report := "7TZLVgR29PGDfrMH"
+	fight, _ := client.GetLastFightInfo(report)
+
+	lunar.GenerateOutputPng("", lunar.NewFFLogsClient().GrabReportInfo(report, *fight))
+
+}
